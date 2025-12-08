8d1ebeb69eb58e47b0ef873acd7f23b94e3b9ac7
Alexa Nakanishi <an899@georgetown.edu>
2025-12-08 08:44:45 -0500

    chi sq testing

--- hypothesis_testing/chisq_testing.R
+++ hypothesis_testing/chisq_testing.R
@@ -5,11 +5,12 @@ df <- read_csv("data/cleaned/natality_cleaned.csv")
 
 head(df)
 
-chisq <- function(A){
-  chisq.test(A)
+chisq <- function(A, correct = FALSE){
+  chisq.test(A, correct = correct)
 }
 
 
+set.seed(5100)
 # are different chronic conditions independent of admission to nicu?
 
 # pre exsting diabetes
@@ -51,3 +52,41 @@ chi_tab4
 chi_tab4$expected
 
 
+# eclampsia - needs permutation test
+tab5 <- table(df$rf_ehype, df$ab_nicu, dnn = c("Eclampsia", "NICU Admission"))
+tab5
+
+chi_tab5 <- chisq(tab5)
+chi_tab5
+
+chi_tab5$expected
+chi_tab5$statistic
+
+chi_tab5_perm <- chisq.test(tab5, correct = FALSE, simulate.p.value = TRUE, B = 10000) 
+chi_tab5_perm
+
+
+# no risks 
+tab6 <- table(df$no_risks, df$ab_nicu, dnn = c("No Risks", "NICU Admission"))
+tab6
+
+chi_tab6 <- chisq(tab6)
+chi_tab6
+
+chi_tab6$expected
+
+
+
+
+
+
+# chronic conditions independent of pregnancy delivery route?
+
+# pre existing diabetes - need permutation test
+tab7 <- table(df$rf_pdiab, df$me_rout, dnn = c("Pre-existing Diabetes", "Deliveryt Route"))
+tab7
+
+chi_tab7 <- chisq(tab7)
+chi_tab7
+
+chi_tab7$expected
